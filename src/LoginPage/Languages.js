35dc5d88da8d7007fc50d8470bf2e37dcdb8221a
Ryson Shalvis <rysonshalvis@Rysons-MacBook-Pro.local>
2020-12-30 17:25:12 -0700

    quick detail fixes

--- src/LoginPage/Languages.js
+++ src/LoginPage/Languages.js
@@ -4,7 +4,7 @@ class Languages extends Component {
     render() {
         return (
             <div className="languages-ctn">
-                <h2 className="language">English</h2>
+                <h2 className="selected-language language">English</h2>
                 <h2 className="language">Français (France)</h2>
                 <h2 className="language">Português (Brasil)</h2>
                 <h2 className="language">Italiano</h2>
