be998a794339c9690501c7d9ca67ad4a56bb71a6
Ryson Shalvis <rysonshalvis@Rysons-MacBook-Pro.local>
2021-01-02 17:04:56 -0700

    add finishing touches to login page for tablet mode

--- src/LoginPage/Footer.js
+++ src/LoginPage/Footer.js
@@ -12,6 +12,42 @@ class Footer extends Component {
                     <p className="footer">More</p>
                     <p className="trademark">Facebook Inc.</p>
                 </div>
+                <div className="tablet-page-break tablet"></div>
+                <div className="footer-tablet tablet">
+                    <p className="footer-more-tablet">SignUp</p>
+                    <p className="footer-more-tablet">Log In</p>
+                    <p className="footer-more-tablet">Messenger</p>
+                    <p className="footer-more-tablet">Facebook Lite</p>
+                    <p className="footer-more-tablet">Watch</p>
+                    <p className="footer-more-tablet">People</p>
+                    <p className="footer-more-tablet">Pages</p>
+                    <p className="footer-more-tablet">Page Categories</p>
+                    <p className="footer-more-tablet">Places</p>
+                    <p className="footer-more-tablet">Games</p>
+                    <p className="footer-more-tablet">Locations</p>
+                    <p className="footer-more-tablet">Marketplace</p>
+                    <p className="footer-more-tablet">Facebook Pay</p>
+                    <p className="footer-more-tablet">Groups</p>
+                    <p className="footer-more-tablet">Jobs</p>
+                    <p className="footer-more-tablet">Oculus</p>
+                    <p className="footer-more-tablet">Portal</p>
+                    <p className="footer-more-tablet">Instagram</p>
+                    <p className="footer-more-tablet">Local</p>
+                    <p className="footer-more-tablet">Fundraisers</p>
+                    <p className="footer-more-tablet">Servics</p>
+                    <p className="footer-more-tablet">Voting Information Center</p>
+                    <p className="footer-more-tablet">About</p>
+                    <p className="footer-more-tablet">Create Ad</p>
+                    <p className="footer-more-tablet">Create Page</p>
+                    <p className="footer-more-tablet">Developers</p>
+                    <p className="footer-more-tablet">Careers</p>
+                    <p className="footer-more-tablet">Privacy</p>
+                    <p className="footer-more-tablet">Cookies</p>
+                    <p className="footer-more-tablet">Ad Choices</p>
+                    <p className="footer-more-tablet">Terms</p>
+                    <p className="footer-more-tablet">Help</p>
+                <p className="tablet-trademark tablet">Facebook © 2021</p>
+                </div>
             </div>
         );
     }
--- src/LoginPage/Languages.js
+++ src/LoginPage/Languages.js
@@ -12,22 +12,24 @@ class Languages extends Component {
                     <h2 className="language">Español</h2>
                     <h2 className="language">中文(简体)</h2>
                     <h2 className="language">Deutsch</h2>
-                    <div className="plus-icon">+</div>
+                    <div className="plus-icon-ctn"><div className="plus-icon"></div></div>
                 </div>
+                
                 <div className="tablet-languages-ctn">
                     <h2 className="selected-language tablet-language">English</h2>
-                    <h2 className="tablet-language">English</h2>
-                    <h2 className="tablet-language">English</h2>
-                    <h2 className="tablet-language">English</h2>
-                    <h2 className="tablet-language">English</h2>
-                    <h2 className="tablet-language">English</h2>
-                    <h2 className="tablet-language">English</h2>
-                    <h2 className="tablet-language">English</h2>
-                    <h2 className="tablet-language">English</h2>
-                    <h2 className="tablet-language">English</h2>
-                    <h2 className="tablet-language">English</h2>
-                    <div className="plus-icon">+</div>
+                    <h2 className="tablet-language">Español</h2>
+                    <h2 className="tablet-language">Français (France)</h2>
+                    <h2 className="tablet-language">中文(简体)</h2>
+                    <h2 className="tablet-language">العربية</h2>
+                    <h2 className="tablet-language">Português (Brasil)</h2>
+                    <h2 className="tablet-language">한국어</h2>
+                    <h2 className="tablet-language">Italiano</h2>
+                    <h2 className="tablet-language">Deutsch</h2>
+                    <h2 className="tablet-language">हिन्दी</h2>
+                    <h2 className="tablet-language">日本語</h2>
+                    <div className="plus-icon-ctn tablet"><div className="plus-icon tablet"></div></div>
                 </div>
+                
             </div>
         );
     }
